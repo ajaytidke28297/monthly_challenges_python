b33131c5910c21615067ad64084fbdd68836e56d
Ajay tidke <ajay.tidke@elastic.run>
2022-10-05 15:15:29 +0530

    more urls and views

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:month>", views.monthly_challenge_by_number),
+    path("<int:month>", views.monthly_challenge_by_number, name="month-challenge"),
     path("<str:month>", views.monthly_challenge),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,7 @@ from django.http import (
     HttpResponseNotFound,
     HttpResponsePermanentRedirect,
 )
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month",
@@ -22,13 +23,18 @@ monthly_challenges = {
 # Create your views here.
 
 
+def index(request):
+    return HttpResponse()
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
         return HttpResponseNotFound("Invalid Month")
 
     redirect_month = months[month - 1]
-    return HttpResponsePermanentRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponsePermanentRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
